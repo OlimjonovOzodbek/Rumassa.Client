623ee942c3155705179be2ea6ba47e9f4afacd93
Ozodbek <145344998+OlimjonovOzodbek@users.noreply.github.com>
2024-05-22 15:29:41 +0500

    Product

--- src/app/components/pages/o-p/o-p-one/o-p-one/o-p-one.component.ts
+++ src/app/components/pages/o-p/o-p-one/o-p-one/o-p-one.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeInNineComponent } from '../../../home/home-in-nine/home-in-nine.component';
 
 @Component({
   selector: 'app-o-p-one',
   templateUrl: './o-p-one.component.html',
-  styleUrl: './o-p-one.component.scss'
+  styleUrl: './o-p-one.component.scss',
+  standalone: true,
+  imports: [HomeInNineComponent]
 })
 export class OPOneComponent {
 
